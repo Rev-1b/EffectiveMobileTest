44a5abeb02a16c258cb6b2117e12e7b73f734460
Alex Luenko <new.luenkoaleksei@gmail.com>
2024-05-05 20:04:30 +0400

    Added show and add func-s

--- handlers.py
+++ handlers.py
@@ -1,13 +1,13 @@
-from languages import get_lang_codes, registered_languages
-from validators import *
+from abc import ABC, abstractmethod
 
 import pandas as pd
 from tabulate import tabulate
+from datetime import datetime
 
-from abc import ABC, abstractmethod
+from languages import get_lang_codes, registered_languages
 
 pd.set_option('display.max_columns', None)
-paginate_by = 5
+PAGINATE_BY = 5
 
 
 class AbstractHandler(ABC):
@@ -18,13 +18,15 @@ class AbstractHandler(ABC):
     def operate(self):
         pass
 
-    @abstractmethod
     def _validate_input(self, *args, **kwargs) -> str:
         """
 
         Requires user to enter the correct command until it gets it.
 
         """
+        if len(args) < 2:
+
+
         initial_message, options = args[:2]
         input_command = input(initial_message).strip()
 
@@ -46,9 +48,6 @@ class SetLanguageHandler(AbstractHandler):
 
         return registered_languages.get(chosen_code)
 
-    def _validate_input(self, *args, **kwargs) -> str:
-        return super()._validate_input(*args, **kwargs)
-
 
 class StartHandler(AbstractHandler):
     def operate(self):
@@ -57,9 +56,6 @@ class StartHandler(AbstractHandler):
         show_tutorial = self._validate_input(message, list(options.keys()))
 
         return options[show_tutorial]
-
-    def _validate_input(self, *args, **kwargs) -> str:
-        return super()._validate_input(*args, **kwargs)
 
 
 class ShowTutorialHandler(AbstractHandler):
@@ -83,9 +79,6 @@ class ShowTutorialHandler(AbstractHandler):
             if not options[further]:
                 break
 
-    def _validate_input(self, *args, **kwargs) -> str:
-        return super()._validate_input(*args, *kwargs)
-
 
 class ChooseCommandHandler(AbstractHandler):
     def __init__(self, language: dict[str, str], commands: dict[str]):
@@ -98,5 +91,94 @@ class ChooseCommandHandler(AbstractHandler):
 
         return self.commands.get(command)
 
+
+class ShowStatisticHandler(AbstractHandler):
+    def __init__(self, language: dict[str, str], database_fields: dict[str, str]):
+        super().__init__(language)
+        self.database_fields = database_fields
+
+    def operate(self):
+        """
+
+        The function will request and display the page as requested by
+        the user until the 'exit' command is received.
+        The number of records displayed on an individual page is configured in
+        the paginate_by variable.
+
+        """
+        print(self.language.get('chosen_show_list', '__ERROR__'))
+        df = pd.read_csv('database.csv', index_col='pk')
+
+        if len(df) == 0:
+            print(self.language.get('empty_table', '__ERROR__'))
+            return
+
+        # print('\n' + tabulate(df, headers=['ID', *self.database_fields.values()]), end='\n\n')
+
+        b = df.groupby('type').amount.sum()
+        print(b['income'])
+
+
+class AddNoteHandler(AbstractHandler):
+    def __init__(self, language: dict[str, str], database_fields: dict[str, str]):
+        super().__init__(language)
+        self.database_fields = database_fields
+
+    def operate(self):
+        """
+
+        Prompts the user for the value of all fields contained in
+        the database_field collection.
+        Adds a new entry to the data file based on the received values.
+        For a new entry, the id is obtained by finding the number
+        of entries in the file.
+
+        """
+        print(self.language.get('chosen_add_note', '__ERROR__'))
+        entity = {}
+
+        for field, field_name in self.database_fields.items():
+            if field == 'date':
+                field_value = datetime.now().strftime('%Y-%m-%d')
+            else:
+                message = self.language.get('input_note_data', '__ERROR__').format(field_name=field_name)
+                field_value = self._validate_input(message, field)
+            entity[field] = field_value
+
+        df = pd.read_csv('database.csv', index_col='pk')
+        df.loc[len(df.index)] = entity.values()
+
+        print(self.language.get('note_add_success', '__ERROR__'))
+        print(tabulate(df.tail(1), headers=['ID', *self.database_fields.values()]), end='\n\n')
+
+        df.to_csv('database.csv')
+
     def _validate_input(self, *args, **kwargs) -> str:
-        return super(*args, **kwargs)
+
+
+
+database_fields = {
+    'date': 'Date',
+    'type': 'Operation type',
+    'amount': 'Amount',
+    'descr': 'Description',
+}
+
+# At the moment, filtering fields is only available by the equal sign, however,
+# to add new operations it is enough to register them in this dictionary
+allowed_query_operations = {
+    'date': ('==',),
+    'type': ('==',),
+    'amount': ('==',),
+    'descr': ('==',),
+}
+
+# All commands specified in this collection become available to the user in the main menu
+commands = {
+    'help': ShowTutorialHandler,
+    'sh': ShowStatisticHandler,
+    'add': AddNoteHandler,
+    # 'find_notes': find_notes_handler,
+    # 'edit_notes': edit_notes_handler,
+    # 'exit': exit_handler,
+}
--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-from handlers import SetLanguageHandler, StartHandler, ShowTutorialHandler, ChooseCommandHandler
-from languages import eng_lang
+from handlers import SetLanguageHandler, StartHandler, ShowTutorialHandler, ChooseCommandHandler, commands, \
+    database_fields
+from languages import eng_lang, ru_lang
 
 
 def run(tutorial_steps):
-    select_language_handler = SetLanguageHandler(eng_lang)
-    language = select_language_handler.operate()
+    # select_language_handler = SetLanguageHandler(eng_lang)
+    # language = select_language_handler.operate()
 
-    start_handler = StartHandler(language)
-    show_tutorial = start_handler.operate()
-    if show_tutorial:
-        show_tutorial_handler = ShowTutorialHandler(language, tutorial_steps)
-        show_tutorial_handler.operate()
+    language = ru_lang
+
+    # start_handler = StartHandler(language)
+    # show_tutorial = start_handler.operate()
+    # if show_tutorial:
+    #     show_tutorial_handler = ShowTutorialHandler(language, tutorial_steps)
+    #     show_tutorial_handler.operate()
 
     while True:
-        choose_command_handler = ChooseCommandHandler(language, tutorial_steps)
-        command_class = choose_command_handler.operate()(language)
-        command_class.operate(language)
+        choose_command_handler = ChooseCommandHandler(language, commands)
+        command_class = choose_command_handler.operate()(language, database_fields)
+        command_class.operate()
 
 
 if __name__ == '__main__':
